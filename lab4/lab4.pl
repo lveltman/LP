b6283336085c7283e0498cca70cdb1117bded9bf
linuxxxoid <45791426+linuxxxoid@users.noreply.github.com>
2020-02-05 00:44:45 +0300

    done

---
+++ lab4/lab4.pl
@@ -0,0 +1,265 @@
+parent('Yaroslav Veltman', 'Lina Veltman').
+parent('Tatyana Romanenko', 'Lina Veltman').
+parent('Yaroslav Veltman', 'Velizar Veltman').
+parent('Tatyana Romanenko', 'Velizar Veltman').
+parent('Victor Romanenko', 'Tatyana Romanenko').
+parent('Zoya Maslenkova', 'Tatyana Romanenko').
+parent('Pavel Veltman', 'Yaroslav Veltman').
+parent('Natalya Utkina', 'Yaroslav Veltman').
+parent('Pavel Veltman', 'Elena Veltman').
+parent('Natalya Utkina', 'Elena Veltman').
+parent('Ivan Utkin', 'Natalya Utkina').
+parent('Nina Kovaleva', 'Natalya Utkina').
+parent('Ivan Utkin', 'Yuri Utkin').
+parent('Nina Kovaleva', 'Yuri Utkin').
+parent('Anatoly Maslenkov', 'Zoya Maslenkova').
+parent('Anna Emelkina', 'Zoya Maslenkova').
+parent('Anatoly Maslenkov', 'Galina Maslenkova').
+parent('Anna Emelkina', 'Galina Maslenkova').
+parent('Anatoly Maslenkov', 'Anatol Maslenkov').
+parent('Anna Emelkina', 'Anatol Maslenkov').
+parent('Anatoly Maslenkov', 'Vyacheslav Maslenkov').
+parent('Anna Emelkina', 'Vyacheslav Maslenkov').
+parent('Anatoly Maslenkov', 'Alexandr Maslenkov').
+parent('Anna Emelkina', 'Alexandr Maslenkov').
+parent('Nikolay Veltman', 'Pavel Veltman').
+parent('Evgenia Dobrecova', 'Pavel Veltman').
+parent('Nikolay Veltman', 'Oleg Veltman').
+parent('Evgenia Dobrecova', 'Oleg Veltman').
+parent('Nikolay Veltman', 'Alexandr Veltman').
+parent('Evgenia Dobrecova', 'Alexandr Veltman').
+parent('Grigory Romanenko', 'Victor Romanenko').
+parent('Valentina Romanenko', 'Victor Romanenko').
+parent('Grigory Romanenko', 'Lida Romanenko').
+parent('Valentina Romanenko', 'Lida Romanenko').
+parent('Dmitry Biryukov', 'Ulyana Biryukova').
+parent('Elena Veltman', 'Ulyana Biryukova').
+parent('Oleg Veltman', 'Gleb Veltman').
+parent('Gulnara Bakaeva', 'Gleb Veltman').
+parent('Alexandr Veltman', 'Dmitry Veltman').
+parent('Larisa Veltman', 'Dmitry Veltman').
+parent('Victor Alishkin', 'Pavel Alishkin').
+parent('Galina Maslenkova', 'Pavel Alishkin').
+parent('Vyacheslav Maslenkov', 'Anna Maslenkova').
+parent('Natalya Kozyr', 'Anna Maslenkova').
+parent('Anatol Maslenkov', 'Alexey Maslenkov').
+parent('Alla Kuznecova', 'Alexey Maslenkov').
+parent('Anatol Maslenkov', 'Anastasia Maslenkova').
+parent('Alla Kuznecova', 'Anastasia Maslenkova').
+parent('Gleb Veltman', 'Elizaveta Veltman').
+parent('Lira Bagirova', 'Elizaveta Veltman').
+parent('Gleb Veltman', 'Lev Veltman').
+parent('Lira Bagirova', 'Lev Veltman').
+parent('Alexandr Maslenkov', 'Sergey Maslenkov').
+parent('Svetlana Lvova', 'Sergey Maslenkov').
+parent('Alexandr Maslenkov', 'Tatyana Maslenkova').
+parent('Svetlana Lvova', 'Tatyana Maslenkova').
+parent('Elena Tuz', 'Alexey Maslenkov').
+parent('Elena Tuz', 'Anastasia Maslenkova').
+parent('Mihail Emelkin', 'Anna Emelkina').
+parent('Marfa Ivanova', 'Anna Emelkina').
+parent('Mihail Emelkin', 'Evdokia Emelkina').
+parent('Marfa Ivanova', 'Evdokia Emelkina').
+parent('Mihail Emelkin', 'Ekaterina Emelkina').
+parent('Marfa Ivanova', 'Ekaterina Emelkina').
+parent('Mihail Emelkin', 'Alexey Emelkin').
+parent('Marfa Ivanova', 'Alexey Emelkin').
+parent('Pavel Alishkin', 'Timur Alishkin').
+parent('Elena Butrimova', 'Timur Alishkin').
+parent('Vadim Kovalev', 'Artem Kovalev').
+parent('Elena Butrimova', 'Artem Kovalev').
+sex('Nina Kovaleva', 'female').
+sex('Ivan Utkin', 'male').
+sex('Anna Emelkina', 'female').
+sex('Anatoly Maslenkov', 'male').
+sex('Galina Maslenkova', 'female').
+sex('Anatol Maslenkov', 'male').
+sex('Vyacheslav Maslenkov', 'male').
+sex('Alexandr Maslenkov', 'male').
+sex('Yuri Utkin', 'male').
+sex('Nikolay Veltman', 'male').
+sex('Grigory Romanenko', 'male').
+sex('Valentina Romanenko', 'female').
+sex('Ulyana Biryukova', 'female').
+sex('Dmitry Biryukov', 'male').
+sex('Oleg Veltman', 'male').
+sex('Alexandr Veltman', 'male').
+sex('Gleb Veltman', 'male').
+sex('Dmitry Veltman', 'male').
+sex('Larisa Veltman', 'female').
+sex('Evgenia Dobrecova', 'female').
+sex('Pavel Alishkin', 'male').
+sex('Victor Alishkin', 'male').
+sex('Anna Maslenkova', 'female').
+sex('Natalya Kozyr', 'female').
+sex('Alla Kuznecova', 'female').
+sex('Gulnara Bakaeva', 'female').
+sex('Lira Bagirova', 'female').
+sex('Elizaveta Veltman', 'female').
+sex('Elena Tuz', 'female').
+sex('Svetlana Lvova', 'female').
+sex('Sergey Maslenkov', 'male').
+sex('Tatyana Maslenkova', 'female').
+sex('Alexey Maslenkov', 'male').
+sex('Anastasia Maslenkova', 'female').
+sex('Mihail Emelkin', 'male').
+sex('Marfa Ivanova', 'female').
+sex('Lev Veltman', 'male').
+sex('Elena Butrimova', 'female').
+sex('Vadim Kovalev', 'male').
+sex('Artem Kovalev', 'male').
+sex('Timur Alishkin', 'male').
+sex('Lida Romanenko', 'female').
+sex('Evdokia Emelkina', 'female').
+sex('Ekaterina Emelkina', 'female').
+sex('Alexey Emelkin', 'male').
+sex('Yaroslav Veltman', 'male').
+sex('Tatyana Romanenko', 'female').
+sex('Lina Veltman', 'female').
+sex('Zoya Maslenkova', 'female').
+sex('Victor Romanenko', 'male').
+sex('Natalya Utkina', 'female').
+sex('Pavel Veltman', 'male').
+sex('Velizar Veltman', 'male').
+sex('Elena Veltman', 'female').
+
+
+sibling(Sibling, Person):-
+parent(P, Person),
+parent(P, Sibling),
+Person \= Sibling.
+
+cousin(Cousin, Person):-
+parent(P1, Person),
+parent(P2, Cousin),
+sibling(P1, P2),
+sex(Cousin, 'male'),
+Person \= Cousin.
+
+% Степень родства
+
+check_link(Mother, Child, mother) :-
+parent(Mother, Child),
+sex(Mother, 'female').
+
+check_link(Father, Child, father) :-
+parent(Father, Child),
+sex(Father, 'male').
+
+check_link(Daughter, Parent, daughter) :-
+parent(Parent, Daughter),
+sex(Daughter, 'female').
+
+check_link(Son, Parent, son) :-
+parent(Parent, Son),
+sex(Son, 'male').
+
+check_link(Sister, Y, sister) :-
+sibling(Y, Sister),
+sex(Sister, 'female').
+
+check_link(Brother, Y, brother) :-
+sibling(Y, Brother),
+sex(Brother, 'male').
+
+check_link(Wife, Husband, wife) :-
+parent(Wife, Child),
+parent(Husband, Child),
+Husband \= Wife,
+sex(Wife, 'female').
+
+check_link(Husband, Wife, husband) :-
+parent(Wife, Child),
+parent(Husband, Child),
+Husband \= Wife,
+sex(Husband, 'male').
+
+
+check_relation(X) :- member(X, [father, mother, sister, brother, daughter, son, wife, husband, sisters, brothers, sons, daughters]).
+
+
+relatives(X) :- member(X, [papy, mamy, sestry, bratya, dochery, synovya, zheny, muzhya, papa, mama, sestra, brat, doch, syn, zhena, muzh]).
+
+famber(sister, sestra).
+famber(brother, brat).
+famber(son, syn).
+famber(daughter, doch).
+
+famber(wife, zhena).
+famber(husband, muzh).
+famber(mother, mama).
+famber(father, papa).
+famber(sister, sisters).
+famber(brother, brothers).
+famber(son, sons).
+famber(daughter, daughters).
+
+famber(sister, sestry).
+famber(brother, bratya).
+famber(son, synovya).
+famber(daughter, dochery).
+famber(wife, zheny).
+famber(husband, muzhya).
+famber(mother, mamy).
+famber(father, papy).
+
+ask_relative(X , Y, Res):- % цепочка родства, через которую связаны 2 человека
+    check_relation(Res),!,
+    check_link(X, Y, Res).
+
+
+question_word(X) :- member(X, [who, whose, chei, chya, kto]).
+
+verb(X) :- member(X, [is, are]).
+
+apostrophe(X) :- member(X, ["'s"]).
+
+question_mark(X) :- member(X, ['?']).
+
+
+/* kto '<name>' relation ? */
+answer(List, Res) :-
+	List = [A, B, C, D],
+	question_word(A), relatives(C),
+    famber(C1, C), question_mark(D),
+    setof(X, ask_relative(X, B, C1), Res).
+
+
+/* chei/chya <relation> '<name>' ? */
+answer(List, Res) :-
+	List = [A, B, C, D],
+	question_word(A), relatives(B),
+    famber(B1, B), question_mark(D),
+    setof(X, ask_relative(C, X, B1), Res).
+
+
+/* '<name>' relation '<name>' ? */
+answer(List, Res) :-
+	List = [A, B, C, D],
+	relatives(B), famber(B1, B),
+	question_mark(D), check_link(A, C, B1), !, Res = yes.
+
+
+/* who is/are '<name>' 's <relation> ? */
+answer(List, Res) :-
+    List = [A, B, C, D, E, F],
+    question_word(A), verb(B), apostrophe(D),
+    check_relation(E), famber(E1, E),question_mark(F),
+    setof(X, ask_relative(X, C, E1), Res).
+
+
+
+/* whose <relation> is '<name>' ? */
+answer(List, Res) :-
+    List = [A, B, C, D, E],
+    question_word(A), check_relation(B), verb(C),
+    question_mark(E),
+    setof(X, ask_relative(D, X, B), Res).
+
+
+/* is '<name>' '<name>' 's <relation> ? */
+answer(List, Res) :-
+    List = [A, B, C, D, E, F], verb(A),
+    apostrophe(D), check_relation(E),
+    question_mark(F), check_link(B, C, E), !, Res = yes. 
+
